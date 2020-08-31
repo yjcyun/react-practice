6bc53223975abf7890108b8868f76afdd4a6d301
Christina Yujeong Yun <yjc.yun@gmail.com>
2020-08-30 22:31:56 -0400

    Updates

---
+++ precache-manifest.87ba2d58a0ac6289af533d5cceb20df5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddd9f983cbd59645ff207f99f3e7a52d",
+    "url": "/yjcyun/react-practice.git/index.html"
+  },
+  {
+    "revision": "cb4ceee459d027a01f43",
+    "url": "/yjcyun/react-practice.git/static/js/2.3943cda5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/yjcyun/react-practice.git/static/js/2.3943cda5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "011da5f1af884bffa8e7",
+    "url": "/yjcyun/react-practice.git/static/js/main.83810207.chunk.js"
+  },
+  {
+    "revision": "16a46106cbfbfc32ed69",
+    "url": "/yjcyun/react-practice.git/static/js/runtime-main.dbc1721b.js"
+  }
+]);
