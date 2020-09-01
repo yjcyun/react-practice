424b7f2d7be8d85591b458703b7e895e4cfcc2f3
Christina Yujeong Yun <yjc.yun@gmail.com>
2020-09-01 13:48:47 -0400

    Write streamReducer and connect to StreamList

---
+++ sample.js
@@ -0,0 +1,25 @@
+// Array based approach
+const streamReducer = (state = [], action) => {
+  switch (action.type) {
+    case EDIT_STREAM:
+      return state.map(stream => {
+        if (stream.id === action.payload.id) {
+          return action.payload;
+        } else {
+          return stream;
+        }
+      })
+    default:
+      return state;
+  }
+}
+
+// object based
+const streamReducer = (state = {}, action) => {
+  switch (action.type) {
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload }
+    default:
+      return state;
+  }
+}
--- src/actions/index.js
+++ src/actions/index.js
@@ -22,30 +22,37 @@ export const signOut = () => {
     type: SIGN_OUT
   }
 }
+
 // to create stream
-export const createStream = formValues => async dispatch => {
-  const response = await streams.post('/streams', formValues);
+export const createStream = formValues => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post('/streams', { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  // navigate back to stream lists
 }
+
 //list all records
 export const fetchStreams = () => async dispatch => {
   const response = await streams.get('/streams');
 
   dispatch({ type: FETCH_STREAMS, payload: response.data });
 }
+
 // list ONE record
 export const fetchStream = id => async dispatch => {
   const response = await streams.get(`/streams/${id}`);
 
   dispatch({ type: FETCH_STREAM, payload: response.data });
 }
+
 // edit ONE stream
 export const editStream = (id, formValues) => async dispatch => {
   const response = await streams.put(`/streams/${id}`, formValues);
 
   dispatch({ type: EDIT_STREAM, payload: response.data });
 }
+
 // delete ONE stream
 export const deleteStream = id => async dispatch => {
   await streams.delete(`/streams/${id}`);
