2a8c1cc12f3a9277dbcac88ce506b9b9172c5ea3
Christina Yujeong Yun <yjc.yun@gmail.com>
2020-09-01 09:59:31 -0400

    Include userId to GoogleAuth

--- src/components/GoogleAuth.jsx
+++ src/components/GoogleAuth.jsx
@@ -18,7 +18,7 @@ class GoogleAuth extends Component {
   // Update state 
   onAuthChange = (isSignedIn) => {
     if (isSignedIn) {
-      this.props.signIn();
+      this.props.signIn(this.auth.currentUser.get().getId());
     } else {
       this.props.signOut()
     }
